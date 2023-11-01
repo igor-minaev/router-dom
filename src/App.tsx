a342265948d5fa226a34f6391a48bccde1024515
Igor Minayeu <igor.minayeu@gmail.com>
2023-11-01 22:12:14 +0300

    01 - Введение в react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <NavLink to='/'>-Main- </NavLink>
+                <NavLink to='/profile'>-Profile- </NavLink>
+                <NavLink to='/login'>-Login- </NavLink>
+            </div>
+            <Routes>
+                <Route path='/' element={<div>Main</div>}/>
+                <Route path='/profile' element={<div>Profile</div>}/>
+                <Route path='/login' element={<div>Login</div>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
