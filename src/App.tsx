bac62dbaef0e9defb5749d677e030568970b8f89
Igor Minayeu <igor.minayeu@gmail.com>
2023-11-01 22:45:28 +0300

    02 - Вложенности

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {NavLink, Route, Routes} from "react-router-dom";
+import {NavLink, Outlet, Route, Routes} from "react-router-dom";
 
 function App() {
     return (
@@ -9,11 +9,33 @@ function App() {
                 <NavLink to='/'>-Main- </NavLink>
                 <NavLink to='/profile'>-Profile- </NavLink>
                 <NavLink to='/login'>-Login- </NavLink>
+                <NavLink to='/profile/settings'>-Settings- </NavLink>
             </div>
             <Routes>
+                <Route path='/*' element={<div>Error 404</div>}/>
                 <Route path='/' element={<div>Main</div>}/>
-                <Route path='/profile' element={<div>Profile</div>}/>
-                <Route path='/login' element={<div>Login</div>}/>
+                {/*1 variant*/}
+                {/*<Route path='/profile/*' element={*/}
+                {/*    <div>*/}
+                {/*        Profile*/}
+                {/*        <Routes>*/}
+                {/*            <Route path='/settings' element={<div>settings</div>}/>*/}
+                {/*        </Routes>*/}
+                {/*    </div>*/}
+                {/*}/>*/}
+                {/*2 variant*/}
+                <Route path='/profile' element={(
+                    <div>
+                        Profile
+                        <Outlet/>
+                    </div>)}>
+                    <Route path='*' element={<div>Profile page not found</div>}/>
+                    <Route path=':id/' element={<div>Id</div>}/>
+                    {/*<Route path='/profile/settings' element={<div>settings</div>}/>*/}
+                    <Route path='settings' element={<div>settings</div>}/>
+                </Route>
+
+                <Route path='/login/*' element={<div>Login</div>}/>
             </Routes>
         </div>
     );
