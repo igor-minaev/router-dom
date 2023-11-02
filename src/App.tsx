41f690a3c1d7ee1d3ff764cdb92ded67278c3fd0
Igor Minayeu <igor.minayeu@gmail.com>
2023-11-02 22:00:17 +0300

    03 - Links style

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,22 @@ import React from 'react';
 import './App.css';
 import {NavLink, Outlet, Route, Routes, useParams} from "react-router-dom";
 
-const Profile = () => {
-    const params = useParams<'*'>()
-    const some = params['*']
-    console.log(some)
-    return <div>Profile</div>
-}
+
 
 function App() {
     return (
         <div className="App">
             <div>
                 <NavLink to='/'>-Main- </NavLink>
-                <NavLink to='/profile'>-Profile- </NavLink>
+                <NavLink style={{color:'lime'}} to='/profile'>-Profile- </NavLink>
                 <NavLink to='/login'>-Login- </NavLink>
-                <NavLink to='/profile/1'>-Profile/1- </NavLink>
+                <NavLink to='/profile/settings'>-Settings- </NavLink>
             </div>
             <Routes>
                 <Route path='/*' element={<div>Error 404</div>}/>
                 <Route path='/' element={<div>Main</div>}/>
-                <Route path='/profile/*' element={<Profile/>}/>
+                <Route path='/profile' element={<div>Profile</div>}/>
+                <Route path='/profile/settings' element={<div>Settings</div>}/>
                 <Route path='/login/*' element={<div>Login</div>}/>
             </Routes>
         </div>
