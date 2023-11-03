8f23dbdbbfd39275cb6018699b741b6f57741daf
Igor Minayeu <igor.minayeu@gmail.com>
2023-11-03 20:50:39 +0300

    06 - useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import {Navigate, NavLink, Outlet, Route, Routes, useNavigate, useParams} from "react-router-dom";
+import {Navigate, NavLink, Outlet, Route, Routes, useNavigate, useParams, useSearchParams} from "react-router-dom";
 
 
 const Profile = () => {
-    const navigate = useNavigate()
 
-    // useEffect(() => {
-    //     if (true) navigate('/login')
-    // })
+    const [searchParams, setSearchParams] = useSearchParams()
+    console.log(searchParams.get('name'))
+    console.log(Object.fromEntries(searchParams))
 
     return (
         <div>
-            {true && <Navigate to={'/login'}/>}
             profile
             <button onClick={() => {
-                navigate('/login')
-            }}>logout</button>
+                setSearchParams({age: '32'})
+            }}>add age</button>
         </div>
     )
 }
@@ -29,13 +27,11 @@ function App() {
                 <NavLink to='/'>-Main- </NavLink>
                 <NavLink to='/profile'>-Profile- </NavLink>
                 <NavLink to='/login'>-Login- </NavLink>
-                <NavLink to='/profile/settings'>-Settings- </NavLink>
             </div>
             <Routes>
                 <Route path='/*' element={<div>Error 404</div>}/>
                 <Route path='/' element={<div>Main</div>}/>
                 <Route path='/profile' element={<Profile/>}/>
-                <Route path='/profile/settings' element={<div>Settings</div>}/>
                 <Route path='/login/' element={<div>Login</div>}/>
             </Routes>
         </div>
